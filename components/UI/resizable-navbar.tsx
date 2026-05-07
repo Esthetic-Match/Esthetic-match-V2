a9fb1da3da6c6af6b0f65e9f87d34424dbc1fadf
osbadawy <omar.badawy@hotmail.com>
2026-05-07 19:04:40 +0300

    link hotfix

--- components/UI/resizable-navbar.tsx
+++ components/UI/resizable-navbar.tsx
@@ -7,7 +7,7 @@ import {
   useScroll,
   useMotionValueEvent,
 } from "motion/react";
-
+import { Link } from "@/i18n/navigation";
 import React, { useState } from "react";
 
 
@@ -115,7 +115,7 @@ export const NavItems = ({ items, className, onItemClick }: NavItemsProps) => {
       )}
     >
       {items.map((item, idx) => (
-        <a
+        <Link
           onMouseEnter={() => setHovered(idx)}
           onClick={onItemClick}
           className="relative px-4 py-2 text-neutral-600 dark:text-neutral-400"
@@ -129,7 +129,7 @@ export const NavItems = ({ items, className, onItemClick }: NavItemsProps) => {
             />
           )}
           <span className="relative z-20">{item.name}</span>
-        </a>
+        </Link>
       ))}
     </motion.div>
   );
@@ -222,7 +222,7 @@ export const MobileNavToggle = ({
 
 export const NavbarLogo = () => {
   return (
-    <a
+    <Link
       href="/"
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
     >
@@ -232,7 +232,7 @@ export const NavbarLogo = () => {
         width={25}
         height={25}
       />
-    </a>
+    </Link>
   );
 };
 
