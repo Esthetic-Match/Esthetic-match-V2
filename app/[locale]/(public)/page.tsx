5ee47079b96341c70fa8edf5958d93d10ca4f2f6
osbadawy <omar.badawy@hotmail.com>
2026-06-20 16:21:27 +0200

    added geo location back

--- app/[locale]/(public)/page.tsx
+++ app/[locale]/(public)/page.tsx
@@ -52,7 +52,7 @@ export default function HomePage() {
             <HomeSection />
             <CategoryCarousel/>
             <ProfileDisplay />
-            {/* <DoctorsNearMeClient /> */}
+            <DoctorsNearMeClient />
             <WhyPatientsUseEstheticMatch />
             <WhyDoctorsShouldJoin />
             <FinalStatement/>
