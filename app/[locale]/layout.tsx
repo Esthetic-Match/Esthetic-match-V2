643f5ca809d62efbc76010e69a105322a1963612
osbadawy <omar.badawy@hotmail.com>
2026-06-22 15:26:10 +0200

    added google analytics

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import Script from "next/script";
 
 export default async function LocaleLayout({
@@ -12,6 +13,7 @@ export default async function LocaleLayout({
   params: Promise<{ locale: string }>;
 }) {
   const { locale } = await params;
+  const gaId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
 
   if (!routing.locales.includes(locale as "en" | "fr")) {
     notFound();
@@ -28,6 +30,7 @@ export default async function LocaleLayout({
         data-blockingmode="auto"
         strategy="beforeInteractive"
       />
+      {gaId ? <GoogleAnalytics gaId={gaId} /> : null}
 
       <NextIntlClientProvider messages={messages}>
         {children}
